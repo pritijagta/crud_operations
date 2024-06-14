723ecb5223d7b7dac6db67bbb38ae99a02b838f3
Priti <pritijagtap242@gmail.com>
2024-06-14 18:05:12 +0530

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const path=require("path")
 const app=express();
 const userModel=require("./models/user")
-
+const port=3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
